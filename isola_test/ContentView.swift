ae44b334ecaaed758b41d3b4a2b219b6745bad64
n <nancy6lin@gmail.com>
2026-03-30 14:46:21 +0800

    Initial Commit

---
+++ isola_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  isola_test
+//
+//  Created by Biu on 2026/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ isola_test/isola_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  isola_testApp.swift
+//  isola_test
+//
+//  Created by Biu on 2026/3/30.
+//
+
+import SwiftUI
+
+@main
+struct isola_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
