2eea2e8afebd8f959c039a7284cb085342686716
Qian <y143m.ray@gmail.com>
2026-06-10 18:33:57 +0800

    delete_unable_file

--- isola_test/Content/isola_testApp.swift
+++
@@ -1,3 +0,0 @@
-// 此檔案為舊的 App 入口點，已停用。
-// 實際的 @main 在 Content/page/ContentView.swift 的 YourApp。
-// 可在 Xcode 中右鍵點擊此檔案 → Delete 以清除。
--- isola_test/Model/Question.swift
+++
@@ -1,2 +0,0 @@
-// 此檔案中的 Question struct 已被 JournalQuestion (@Model) 取代，無任何引用。
-// 可在 Xcode 中右鍵點擊此檔案 → Delete 以清除。
